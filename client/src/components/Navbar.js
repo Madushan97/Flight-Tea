c972655fef5b28db3898e1b56e77e7faf2a65d8d
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-02-04 18:25:49 +0530

    Booking a room, booking model

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,20 +17,20 @@ function Navbar() {
                 <div className="container-fluid">
                 <a className="navbar-brand" href="#">Flight Tea</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"><i class="fa fa-bars" style={{color:'white'}}></i></span>
+                    <span className="navbar-toggler-icon"><i className="fa fa-bars" style={{color:'white'}}></i></span>
                 </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav mr-5">
 
                         {user ? (<>
                             
-                            <div class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                    <i className='fa fa-user'></i>{user.name}
                                 </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="#">Bookings</a>
-                                    <a class="dropdown-item" href="#" onClick={logout}>Log Out</a>                                   
+                                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a className="dropdown-item" href="#">Bookings</a>
+                                    <a className="dropdown-item" href="#" onClick={logout}>Log Out</a>                                   
                                 </div>
                             </div>
 
--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -12,17 +12,21 @@ function Bookingscreen({ match }) {
     const[room, setroom] = useState();
 
     const roomid = match.params.roomid
+    const rentperday = match.params.rentperday
     const fromdate = moment(match.params.fromdate, 'DD-MM-YYYY')
     const todate = moment(match.params.todate, 'DD-MM-YYYY')
    
     // different between 2 days
-    const totaldays = moment.duration(todate.diff(fromdate)).asDays()+1
+    const totaldays = moment.duration(todate.diff(fromdate)).asDays()+1;
+    
+    const[totalamount, settotalamount] = useState()
 
     useEffect(async() => {
 
         try {
             setloading(true);            
             const data = (await axios.post('/api/rooms/getroombyid', {roomid : match.params.roomid})).data;
+            settotalamount(data.rentperday * totaldays)
             setroom(data);
             setloading(false);
             
@@ -31,6 +35,26 @@ function Bookingscreen({ match }) {
             setloading(false)
         }
     }, []);
+
+    async function bookRoom() {
+
+        const bookingDetails = {
+
+            room,
+            userid: JSON.parse(localStorage.getItem('currentUser'))._id,
+            fromdate,
+            todate,
+            totalamount,
+            totaldays
+        }
+
+        try {
+            const result = await axios.post('/api/bookings/bookroom', bookingDetails)
+
+        } catch (error) {
+            
+        }
+    }
 
   return (
     <div className='m-5'>
@@ -50,7 +74,7 @@ function Bookingscreen({ match }) {
 
                     <div>
                         <b>
-                        <p>Name :</p>
+                        <p>Name :{JSON.parse(localStorage.getItem('currentUser')).name}</p>
                         <p>From Date : {match.params.fromdate}</p>
                         <p>To Date : {match.params.todate}</p>
                         <p>Max Count : {room.maxcount}</p>
@@ -64,12 +88,12 @@ function Bookingscreen({ match }) {
                         <b>
                         <p>Total Days : {totaldays}</p>
                         <p>Rent per Day : {room.rentperday}</p>
-                        <p>Total Amount :</p>                        
+                        <p>Total Amount : {totalamount}</p>                        
                         </b>
                     </div>
 
                     <div style={{float:'right'}}>
-                        <button className='btn btn-success'>Pay Now</button>
+                        <button className='btn btn-success' onClick={bookRoom}>Pay Now</button>
                     </div>
 
                 </div>
